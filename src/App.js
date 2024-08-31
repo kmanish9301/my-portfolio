efc6fb821bc76a58ea0694438dbbca4014233fc0
kmanish9301 <manishkharbade09032001@gmail.com>
2024-08-31 19:45:21 +0530

    Home section changes for the portfolio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Navbar from './Components/navbar/Navbar'
-import Skills from './Components/skillsBanner/Skills'
-import Experience from './Components/experience/Experience'
-import Projects from './Components/projects/Projects'
-import Contact from './Components/contact/Contact'
-import About from './Components/about/About'
+import React from "react";
+import Navbar from "./Components/navbar/Navbar";
+import Skills from "./Components/skillsBanner/Skills";
+import Experience from "./Components/experience/Experience";
+import Projects from "./Components/projects/Projects";
+import Contact from "./Components/contact/Contact";
+import About from "./Components/about/About";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText">
-      <div className='max-w-screen-2xl mx-auto px-16'>
+    <div className="w-full h-auto bg-bodyColor text-lightText relative">
+      <div className="parallax-stars">
+        <div className="stars"></div>
+      </div>
+      <div className="moon"></div>
+      <div className="max-w-screen-xl mx-auto w-full">
         <Navbar />
         <About />
         <Skills />
@@ -18,7 +23,7 @@ const App = () => {
         <Contact />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,19 +1,40 @@
-import React from 'react'
-import { navLinkData } from "../../Constants/index"
-import { Link } from "react-scroll"
+import React from "react";
+import { navLinkData } from "../../Constants/index";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    return (
-        <div id='home' className=' grid-rows-6 h-16 flex items-center justify-between font-titleFont'>
-            <p className="text-lg tracking-wider cursor-pointer"> Manish's Portfolio</p>
+  return (
+    <div
+      id="home"
+      className=" grid-rows-6 h-16 flex items-center justify-between font-titleFont"
+    >
+      <p className="text-lg tracking-wider cursor-pointer">
+        {" "}
+        Manish's Portfolio
+      </p>
 
-            <ul className='flex items-center gap-10'>
-                {navLinkData.length > 0 && navLinkData.map(({ id, title, link }) => (
-                    <li key={id} className='text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300'><Link activeClass='active' to={link} spy={true} smooth={true} offset={-70} duration={500}>{title}</Link></li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+      <ul className="flex items-center gap-10">
+        {navLinkData.length > 0 &&
+          navLinkData.map(({ id, title, link }) => (
+            <li
+              key={id}
+              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300 z-10"
+            >
+              <Link
+                activeClass="active"
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
